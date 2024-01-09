f288469cfd43a3140028878fbb64e234ac64d243
bigbosshong1101 <bigbosshong1101@gmail.com>
2024-01-10 01:17:32 +0900

    PRG Post/Redirect/Get

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -67,13 +67,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4( Item item,
                             Model model) {
         itemRepository.save(item);
 //        model.addAttribute("item", item); //자동 추가, 생략 가능
 
         return "basic/item";
+    }
+
+    /**
+     * PRG - Post/Redirect/Get
+     */
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
